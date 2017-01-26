5df06585829cdef2c394ae4b84e5d4fdc39d3f90
novacane <espinoza.sample@gmail.com>
2017-01-26 12:46:07 -0300

    Sin conexión a base de datos

---
+++ public/js/firebaseConexion.js
@@ -0,0 +1,11 @@
+ function getConfig(){
+ var config = {
+    apiKey: "AIzaSyC9w9tG6nIN1FT7VifxlFOr699fLl8jDYs",
+    authDomain: "gulmue-a0a12.firebaseapp.com",
+    databaseURL: "https://gulmue-a0a12.firebaseio.com",
+    storageBucket: "gulmue-a0a12.appspot.com",
+    messagingSenderId: "827087036430"
+  };
+return config;
+ } 
+ 
