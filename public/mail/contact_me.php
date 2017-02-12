4d6b48ab444cfebcdf034b2b1f75d323a480492a
novacane <espinoza.sample@gmail.com>
2017-02-12 14:57:45 -0300

    cambios mínimos en contacto

--- public/mail/contact_me.php
+++ public/mail/contact_me.php
@@ -6,7 +6,7 @@ if(empty($_POST['name'])  		||
    empty($_POST['message'])	||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
-	echo "No arguments Provided!";
+	echo "Campos vacioes en el formulario de correo!";
 	return false;
    }
 	
@@ -16,7 +16,7 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 	
 // Crear el correo con el mensaje
-$to = 'espinoza.sample@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'carlaespinoza.lopez@hotmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Formulario de contacto Gülmue:  $name";
 $email_body = "Has recibido un nuevo correo desde el formulario de contacto.\n\n"."Aquí el detalle :\n\nNombre: $name\n\nCorreo: $email_address\n\nTelefono: $phone\n\nMensaje:\n$message";
 $headers = "From: gulmue@gulmue-a0a12.firebaseapp.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
